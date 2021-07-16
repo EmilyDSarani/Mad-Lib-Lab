fd73a9a7ab1c5e599ea48bf1bff25eb4f723aa57
Estefani <76499574+HonduranCoder@users.noreply.github.com>
2021-07-16 18:53:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // import functions and grab DOM elements
-
+const nounEl = document.getElementById('noun');  
+const verbEl = document.getElementById('verb');
+const adjectiveEl = document.getElementById('adjective');
+const buttonEl = document.getElementbyId('button');
 // initialize state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+buttonEl.addEventListener('click', ()=>{
+
+});
